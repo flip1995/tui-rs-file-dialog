24160d825a5c6b48d52b49762c9954985000021d
Philipp Krones <hello@philkrones.com>
2023-07-14 22:26:01 +0200

    First failed experiment

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
-fn main() {
-    println!("Hello, world!");
+use crossterm::{
+    execute,
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+};
+use std::{io, thread, time::Duration};
+use tui::{
+    backend::CrosstermBackend,
+    widgets::{Block, Borders},
+    Terminal,
+};
+
+fn main() -> Result<(), io::Error> {
+    enable_raw_mode()?;
+    let mut stdout = io::stdout();
+    execute!(stdout, EnterAlternateScreen)?;
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+
+    terminal.draw(|f| {
+        let mut graph_size = dbg!(f.size());
+        let mut devices_size = f.size();
+        let graph = Block::default().title("Graph").borders(Borders::ALL);
+        let devices = Block::default().title("Devices").borders(Borders::ALL);
+
+        graph_size.width /= 2;
+        devices_size.y = devices_size.width / 2;
+        devices_size.width /= 2;
+
+        f.render_widget(graph, graph_size);
+        f.render_widget(devices, dbg!(devices_size));
+    })?;
+
+    thread::sleep(Duration::from_secs(5));
+
+    disable_raw_mode()?;
+    execute!(terminal.backend_mut(), LeaveAlternateScreen)?;
+    terminal.show_cursor()?;
+
+    Ok(())
 }
